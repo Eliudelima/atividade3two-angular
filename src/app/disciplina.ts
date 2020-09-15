ce278b4e5042414f8879b7d5f9122297bf3491b1
Eliudelima <eliudeangelo@gmail.com>
2020-09-15 18:15:10 -0300

    2 files modified

--- src/app/disciplina.ts
+++ src/app/disciplina.ts
@@ -2,55 +2,55 @@ export const disciplina = [
   {
     title:"Criação de Empresas para Internet",
     ementa: "O planejamento de um projeto de negócios para Internet. Implantação e implementação de projetos de negócios. Administração de custos e dos prazos de implantação em projetos. Identificação de oportunidades de investimentos. Analise de mercado. Desenvolvimento e implementação de negócios. Finanças de um projeto de Investimentos. Análise de sensibilidade. Empreendedorismo e inovação: liderança estratégica. Administração participativa e os novos modelos de administração. Principais funções administrativas e respectivos sistemas de informação. Mecanismos e procedimentos para criação de empresas. Criação de empresas focadas na Internet. Computação em Nuvem - estratégias e modelos",
-    dia: "Guns 'n Roses",
-    horario: 1992
+    dia: "Terça-Feira",
+    horario: 19 
   },
   {
      title:"Desenvolvimento para Servidores II",
     ementa: "Uso de algum ambiente de desenvolvimento (framework) em plataforma proprietária: configuração do ambiente, emulação de dispositivos móveis nos computadores pessoais,  interface gráfica, serviços disponíveis, banco de dados nos dispositivos. Desenvolvimento de aplicativos com uso de frameworks",
-    dia: "Guns 'n Roses",
-    horario: 1992
+    dia: "Quinta- Feira",
+    horario: 19
   },
   {
     title:"Inglês VI",
     ementa: "Aprimoramento da compreensão e produção oral e escrita por meio da integração das habilidades linguístico-comunicativas. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos socioculturais da língua inglesa.",
-    dia: "Guns 'n Roses",
-    horario: 1992
+    dia: "Terça-feira",
+    horario: 21
   },
   {
     title:"Arquitetura Orientada a Serviços",
     ementa: "Construção de aplicações por meio da utilização de arquitetura orientada a serviços – SOA (Service-Oriented Architecture) e das principais tecnologias Web Services",
-    dia: "Guns 'n Roses",
-    horario: 1992
+    dia: "Quarta-feira",
+    horario: 19
   },
   {
     title:"Desenvolvimento para Dispositivos Móveis II",
     ementa: "Use yous illusion I",
-    dia: "Guns 'n Roses",
-    horario: 1992
+    dia: "Quinta-feira",
+    horario: 19
   },
   {
     title:"Projeto de Encontrabilidade",
     ementa: "Teoria Geral. Marcação para encontrabilidade. Microformatos. Conteúdo. Rede Social. Estratégias Server-Side. Como evitar armadilhas de encontrabilidade. Pesquisas. Análises",
-    dia: "Guns 'n Roses",
-    horario: 1992
+    dia: "Segunda-feira",
+    horario: 19
   },
   {
     title:"Tópicos Especiais em Sistemas para Internet II",
     ementa: "Recursos e aplicações da tecnologia escolhida",
-    dia: "Guns 'n Roses",
-    horario: 1992
+    dia: "Sexta-feira",
+    horario: 19
   },
    {
     title:"Tópicos Especiais em Sistemas para Internet III",
     ementa: "Recursos e aplicações da tecnologia escolhida",
-    dia: "Guns 'n Roses",
-    horario: 1992
+    dia: "Sábado",
+    horario: 13
   },
    {
     title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
-    ementa: "Elaboração de portfólios e técnicas profissionais de aprese",
-    dia: "Guns 'n Roses",
-    horario: 1992
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    dia: "Sexta-feira",
+    horario: 21
   }
 ]
